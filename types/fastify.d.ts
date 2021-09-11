e083a0592f67cb7b962b00a0496b77eba9985572
Jackie <18378976+Pyrodash@users.noreply.github.com>
2021-09-11 12:16:58 +0200

    Wiped repository

--- types/fastify.d.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '@sakuraapp/shared'
-
-declare module 'fastify' {
-    export interface FastifyRequest {
-        user?: User
-    }
-}
--- types/passport.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'passport/lib/http/request' {
-    interface Request {
-        [key: string]: () => void
-    }
-
-    const req: Request
-
-    export default req
-}
